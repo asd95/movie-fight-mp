c21e60474aec738226359f3c483e6cd1d461cbc4
Artur <arturseu@gmail.com>
2021-07-14 12:58:10 +0300

    change fetch request

--- src/js/server.js
+++ src/js/server.js
@@ -1,5 +1,5 @@
  async function getData(type, text) {
-  const res = await fetch(`http://www.omdbapi.com/?${type}=${text}&apikey=5b10b9a3`);
+  const res = await fetch(`https://www.omdbapi.com/?${type}=${text}&apikey=5b10b9a3`);
   if (!res.ok) {
     throw new Error(`Could not fetch ${res.url}, received ${res.status}`);
   }
